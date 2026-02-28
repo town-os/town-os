565064ed1dd8717f5ff801b15afc261bf2c48417
Erik Hollensbe <git@hollensbe.org>
2026-02-28 14:52:23 -0800

    sigh
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- install-ubuntu-deps.sh
+++ install-ubuntu-deps.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 echo "Installing system packages..."
 sudo apt-get update
-sudo apt-get install -y golang btrfs-progs libsystemd-dev podman
+sudo apt-get install -y golang btrfs-progs libsystemd-dev podman unzip
 
 echo "Installing Bun..."
 curl -fsSL https://bun.sh/install | bash
